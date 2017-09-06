09f7f00d1e36452da6c2baafba9755b3ba471d34
lbognini <lbognini@bouyguestelecom.fr>
2017-09-06 15:33:40 +0200

    Rajout install docker

--- install-docker.sh
+++ install-docker.sh
@@ -7,9 +7,9 @@ echo "=====  Installing docker package..."
 
 curl -fsSL get.docker.com -o /tmp/get-docker.sh
 sh /tmp/get-docker.sh
+
 usermod -aG docker jenkins
 
-echo "=====  Verifying that Docker EE is installed correctly..."
-docker run --rm hello-world
+service docker start
 
 
